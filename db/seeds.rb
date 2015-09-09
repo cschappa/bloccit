2a2645a3a41097fec3ebb5784dc76ff0ebf58c60
Caleb Schappa <cschappa@users.noreply.github.com>
2015-09-08 23:26:36 -0400

    Assignment 32 complete, added unique post and comment via seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,35 @@
 require 'faker'
 
 # Create posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
-    )
+#50.times do
+# Post.create!(
+#    title: Faker::Lorem.sentence,
+#    body: Faker::Lorem.paragraph
+#    )
+#end
+
+if !(Post.find_by(title: "my uniq title"))
+  # it hasn't been seeded
+  Post.create!(title: "my uniq title", body: Faker::Lorem.paragraph)
 end
 
+# what does this line do?
 posts = Post.all
 
 # Create comments
 
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-    )
+#100.times do
+#  Comment.create!(
+#    post: posts.sample,
+#    body: Faker::Lorem.paragraph
+#    )
+#end
+
+if !(Comment.find_by(body: "my uniq comment"))
+  # it hasn't been seeded
+  Comment.create!(post: posts.sample, body: "my uniq comment")
 end
+
 
 puts "Seeds finished"
 puts "#{Post.count} posts created"
