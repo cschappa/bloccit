5957edf3b9bccaae707a2871c0f8c98b74986fe2
Caleb Schappa <cschappa@users.noreply.github.com>
2015-09-25 14:12:41 -0400

    Assignment 43 done - Authorizations

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,24 @@
 class PostPolicy < ApplicationPolicy
+  
+  class Scope
+    # attr_accessor :user, :scope
+    attr_reader :user, :scope
+
+      def initialize (user, scope)
+        @user = user
+        @scope = scope
+      end
+
+      def resolve
+        if user && (user.admin? || user.moderator?)
+          scope.all
+        else
+          scope.where(:user => user)
+        end
+      end 
+
+  end
+
   def index?
     true
   end
